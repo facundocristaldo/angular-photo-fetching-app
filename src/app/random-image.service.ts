ef7d58e6f34a33c7f2a9f1a99b2f8e46406c9ea5
Facundo Cristaldo <fcristaldo@altimetrik.com>
2021-04-20 09:00:55 -0300

    refactor authentication

--- src/app/random-image.service.ts
+++ src/app/random-image.service.ts
@@ -16,7 +16,12 @@ export class RandomImageService {
   constructor(private http:HttpClient) { }
 
   getRandomImage(){
-    return this.http.get<serviceResponse>("https://api.unsplash.com/photos/random?client_id=4l8C6w5dCUoNfsZpxdDXM4-C5YiaIRvFNVuCx5qmCe0")
+    return this.http.get<serviceResponse>("https://api.unsplash.com/photos/random",
+    {
+      headers:{
+        Authorization:"Client-ID 4l8C6w5dCUoNfsZpxdDXM4-C5YiaIRvFNVuCx5qmCe0"
+      }
+    })
     .pipe(
       pluck("urls","full")
     )
